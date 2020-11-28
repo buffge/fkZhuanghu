e014d3492d88e6a75a1fba8f5d5771f5903f3e9c
buffge <1515888956@qq.com>
2020-11-28 16:05:48 +0800

    first commit

---
+++ content.js
@@ -0,0 +1,42 @@
+const sbStrs = ["如何看待", "如何评价"]
+const indexSectionSelector =
+  "#TopstoryContent > div > div > div > .Card.TopstoryItem"
+const indexSectionTitleSelector = ".ContentItem-title  a"
+const hotSectionSelector = ".HotList-list > section"
+const hotSectionTitleSelector = "h2.HotItem-title"
+
+function fkGeneral(sectionSelector, titleSelector) {
+  document.querySelectorAll(sectionSelector).forEach((node) => {
+    let titleNode = node.querySelector(titleSelector)
+    if (titleNode == null) {
+      return
+    }
+    for (const str of sbStrs) {
+      if (titleNode.innerHTML.includes(str)) {
+        node.parentNode.removeChild(node)
+        return
+      }
+    }
+  })
+}
+function fkHome() {
+  this.fkGeneral(indexSectionSelector, indexSectionTitleSelector)
+}
+
+function fkHot() {
+  fkGeneral(hotSectionSelector, hotSectionTitleSelector)
+}
+
+function fk() {
+  switch (location.pathname) {
+    case "/":
+      fkHome()
+      break
+    case "/hot":
+      fkHot()
+      break
+    default:
+      break
+  }
+}
+setInterval(fk, 1000)
